368326e821d90b237f02cdd975df6ae122d03b35
Abhinav <abhinavkinarullathil@gmail.com>
2024-03-08 11:03:21 +0530

    added counter up functionality for the seo section

--- src/Components/Service/SeoMarketing/Seo.jsx
+++ src/Components/Service/SeoMarketing/Seo.jsx
@@ -3,8 +3,12 @@ import Power from "../../../assets/Icons/energetic.png";
 import SeoBanner from "../../../assets/Images/Services/seo-inner-banner.png";
 import Discussion from "../../../assets/Images/Services/office-discussion.jpg";
 import "./seo.css";
+import CountUp from "react-countup";
+import ScrollTrigger from "react-scroll-trigger";
+import { useState } from "react";
 
 export default function Seo() {
+  const [view, setView] = useState(false);
   return (
     <div className="w-full h-full space-y-5">
       {/* Top section */}
@@ -15,12 +19,20 @@ export default function Seo() {
             <h1 data-aos="fade-up">The Ultimate Traffic Boost</h1>
           </div>
           <div>
-            <h1 className="text-2xl md:text-5xl font-bold tracking-wider" data-aos="fade-up" data-aos-delay="50">
+            <h1
+              className="text-2xl md:text-5xl font-bold tracking-wider"
+              data-aos="fade-up"
+              data-aos-delay="50"
+            >
               Unlock The GateWay to Quality Traffic.
             </h1>
           </div>
           <div>
-            <p className="text-xs md:text-sm tracking-wide text-gray-500" data-aos="fade-up" data-aos-delay="80">
+            <p
+              className="text-xs md:text-sm tracking-wide text-gray-500"
+              data-aos="fade-up"
+              data-aos-delay="80"
+            >
               SEO is a digital marketing strategy that helps your website appear
               in relevant search results in search engines.{" "}
             </p>
@@ -72,11 +84,17 @@ export default function Seo() {
       {/* middle section */}
       <div className="w-full h-full flex justify-center pt-10 md:my-10">
         <div className="space-y-4">
-          <div className="text-2xl md:text-5xl text-center md:tracking-wider md:space-y-4" data-aos="fade-up"> 
+          <div
+            className="text-2xl md:text-5xl text-center md:tracking-wider md:space-y-4"
+            data-aos="fade-up"
+          >
             <h1 className="font-bold">Contain Modern features for</h1>
             <h1 className="font-bold">Better Experience</h1>
           </div>
-          <p className="text-sm text-center text-gray-500 px-5 md:px-0" data-aos="fade-up">
+          <p
+            className="text-sm text-center text-gray-500 px-5 md:px-0"
+            data-aos="fade-up"
+          >
             Designed to the last pixel and engineered with unforgiving
             precision, Linear combines UI elegance .
           </p>
@@ -85,10 +103,16 @@ export default function Seo() {
       <div className="bg-transparent seo-middle-container">
         <div className=" bg-transparent shadow-md shadow-violet-500 border border-violet-500 py-10  seo-middle-container-inner">
           <div className=" bg-transparent space-y-5 seo-middle-container-inner-left">
-            <h1 className="text-2xl md:text-4xl font-bold" data-aos="fade-right">
+            <h1
+              className="text-2xl md:text-4xl font-bold"
+              data-aos="fade-right"
+            >
               We follow a simple, yet effective SEO ranking process
             </h1>
-            <p className="text-sm leading-6 text-gray-500" data-aos="fade-right">
+            <p
+              className="text-sm leading-6 text-gray-500"
+              data-aos="fade-right"
+            >
               These days, no business can&apos;t to ignore Search Optimization.
               SEO should plays a part in your online strategy. Search engine
               optimization ensures that your online presence is both articulated
@@ -161,13 +185,19 @@ export default function Seo() {
       {/* grid section */}
       <div className="w-full h-full flex flex-col items-center gap-12 md:py-10">
         <div className="flex text-center">
-          <h1 className="text-2xl md:text-4xl font-bold leading-relaxed" data-aos="fade-up">
+          <h1
+            className="text-2xl md:text-4xl font-bold leading-relaxed"
+            data-aos="fade-up"
+          >
             Unleashing the Power of Advanced <br />
             SEO Tactics
           </h1>
         </div>
         <div>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[0.1rem] bg-slate-500/50" data-aos="fade-up">
+          <div
+            className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[0.1rem] bg-slate-500/50"
+            data-aos="fade-up"
+          >
             {seoData.map((item, i) => (
               <div
                 key={i}
@@ -190,7 +220,12 @@ export default function Seo() {
         <div className="w-full h-full bg-transparent flex flex-col md:flex-row">
           <div className="w-full h-full md:w-1/2 bg-transparent p-6 md:p-16 space-y-6">
             <div>
-              <h2 className="text-2xl md:text-3xl font-semibold" data-aos="fade-up">SEO agency that will evolve your search performance</h2>
+              <h2
+                className="text-2xl md:text-3xl font-semibold"
+                data-aos="fade-up"
+              >
+                SEO agency that will evolve your search performance
+              </h2>
             </div>
             <div className="space-y-4 text-gray-400 ">
               <p className="text-sm md:text-base" data-aos="fade-right">
@@ -206,20 +241,60 @@ export default function Seo() {
               </p>
             </div>
             <div className="flex bg-transparent gap-8" data-aos="fade-right">
-              <div >
-                <h2 className="text-2xl md:text-4xl font-semibold">98%</h2>
+              <div>
+                <h2 className="text-2xl md:text-4xl font-semibold flex">
+                  <ScrollTrigger onEnter={() => setView(true)}>
+                    {view && (
+                      <CountUp
+                        start={0}
+                        end={98}
+                        duration={3}
+                        delay={0}
+                        className="text-2xl md:text-4xl font-semibold"
+                      />
+                    )}
+                  </ScrollTrigger>
+                  <p className="text-2xl md:text-4xl font-semibold">%</p>
+                </h2>
                 <p className="text-xs">Average Conversion Rate</p>
               </div>
               <div>
-                <h2  className="text-2xl md:text-4xl font-semibold">60M+</h2>
-                <p  className="text-xs">Traffic Generated</p>
+                <h2 className="text-2xl md:text-4xl font-semibold flex">
+                  <ScrollTrigger onEnter={() => setView(true)}>
+                    {view && (
+                      <CountUp
+                        start={0}
+                        end={60}
+                        duration={3}
+                        delay={0}
+                        className="text-2xl md:text-4xl font-semibold"
+                      />
+                    )}
+                  </ScrollTrigger>
+                  <p className="text-2xl md:text-4xl font-semibold">M+</p>
+                </h2>
+                <p className="text-xs">Traffic Generated</p>
               </div>
             </div>
-            <div><button className="px-3 py-2 rounded-full ring-1" data-aos="fade-rihgt">Learn More</button></div>
+            <div>
+              <button
+                className="px-3 py-2 rounded-full ring-1"
+                data-aos="fade-right"
+              >
+                Learn More
+              </button>
+            </div>
           </div>
           <div className="w-full h-full md:w-1/2 bg-transparent p-10 md:p-20">
-            <div className="w-full h-full min-w-60  rounded-2xl ring flex justify-center items-center" data-aos="fade-left">
-              <img src={Discussion} alt="discussion image" className="w-full h-full  object-contain rounded-2xl"/>
+            <div
+              className="w-full h-full min-w-60  rounded-2xl ring flex justify-center items-center"
+              data-aos="fade-left"
+            >
+              <img
+                src={Discussion}
+                alt="discussion image"
+                className="w-full h-full  object-contain rounded-2xl"
+              />
             </div>
           </div>
         </div>
