8874cdb79f783b19490ffcadf428a0b877cf45cc
Abhinav <abhinavkinarullathil@gmail.com>
2024-03-13 15:41:33 +0530

    added whatsapp functionality in the contact footer

--- src/Components/Footer/ContactFooter/ContactFooter.jsx
+++ src/Components/Footer/ContactFooter/ContactFooter.jsx
@@ -3,9 +3,15 @@ import { FaWhatsapp,FaPhone } from "react-icons/fa6";
 import { AiOutlineMessage } from "react-icons/ai";
 
 export default function ContactFooter() {
+  function openWhatsApp() {
+    const phoneNumber = import.meta.env.VITE_WHATSAPP_NUMBER;
+    const message = 'Hai';
+    const whatsappUrl = `https://wa.me/${phoneNumber}?text=${message}`;
+    window.open(whatsappUrl, '_blank');
+  }
   return (
     <div className=" w-20 h-fit fixed bottom-10 right-0 bg-transparent flex flex-col justify-between items-center gap-3 py-2">
-        <div className="social-media-content size-10 md:size-12 flex justify-center items-center rounded-full bg-green-500 cursor-pointer">
+        <div className="social-media-content size-10 md:size-12 flex justify-center items-center rounded-full bg-green-500 cursor-pointer" onClick={openWhatsApp}>
             <FaWhatsapp size={25}/>
         </div>
         <div className="social-media-content size-10 md:size-12 flex justify-center items-center rounded-full bg-blue-500 cursor-pointer">
