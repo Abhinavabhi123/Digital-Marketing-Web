cb3b7fcd1145fb1187b12412dc9780f6c5de8345
Abhinav <abhinavkinarullathil@gmail.com>
2024-03-13 15:57:22 +0530

    added telephone functionality

--- src/Components/Footer/ContactFooter/ContactFooter.jsx
+++ src/Components/Footer/ContactFooter/ContactFooter.jsx
@@ -9,13 +9,14 @@ export default function ContactFooter() {
     const whatsappUrl = `https://wa.me/${phoneNumber}?text=${message}`;
     window.open(whatsappUrl, '_blank');
   }
+
   return (
     <div className=" w-20 h-fit fixed bottom-10 right-0 bg-transparent flex flex-col justify-between items-center gap-3 py-2">
         <div className="social-media-content size-10 md:size-12 flex justify-center items-center rounded-full bg-green-500 cursor-pointer" onClick={openWhatsApp}>
             <FaWhatsapp size={25}/>
         </div>
         <div className="social-media-content size-10 md:size-12 flex justify-center items-center rounded-full bg-blue-500 cursor-pointer">
-            <FaPhone size={20}/>
+          <a href={ `tel:${import.meta.env.VITE_WHATSAPP_NUMBER}`}>  <FaPhone size={20}/></a>
         </div>
         <div className="social-media-content size-10 md:size-12 flex justify-center items-center rounded-full bg-yellow-500 cursor-pointer">
             <AiOutlineMessage size={25}/>
