82e73845dc90981d6e9ad37eeef8e7fa90292610
Abhinav <abhinavkinarullathil@gmail.com>
2024-03-13 20:15:04 +0530

    completed portfolio section

--- src/Components/PortFolio/PortFolio.jsx
+++ src/Components/PortFolio/PortFolio.jsx
@@ -10,6 +10,10 @@ export default function PortFolio() {
   function viewHandler(id) {
     setViewDetails(true);
     setData(id - 1);
+    const portfolioDetails = document.getElementById("portfolio-details");
+    if (portfolioDetails) {
+      portfolioDetails.scrollIntoView({ behavior: "smooth" });
+    }
   }
   function handleRemoveDetails() {
     setViewDetails(false);
@@ -22,13 +26,10 @@ export default function PortFolio() {
     setData(data + 1);
   }
 
-  console.log(cards.length, "length");
-  console.log(data, "data");
-
   return (
     <div className=" w-full h-full bg-slate-950 md:pb-20">
       {/* banner section */}
-      <div className="w-full min-h-[30rem] p-10 md:p-20">
+      <div className="w-full min-h-[30rem] p-10 md:p-20" id="portfolio-details">
         <div className="space-y-5">
           <div
             className="text-center text-2xl md:text-5xl font-bold md:space-y-5"
@@ -38,7 +39,7 @@ export default function PortFolio() {
             <h1> Portfolio</h1>
           </div>
           <p
-            className="text-sm md:text-base text-gray-500 md:px-10"
+            className="text-sm md:text-base text-gray-500 md:px-10 text-center"
             data-aos="fade-up"
           >
             Celebrate our journey of innovation and success as we unveil a
@@ -54,20 +55,20 @@ export default function PortFolio() {
         </div>
       </div>
       {!viewDetails ? (
-        <div className="w-full min-h-[40rem] px-36">
+        <div className="w-full min-h-[40rem] p-10 md:px-36">
           <div className="w-full h-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 bg-transparent gap-6">
             {cards.map((item) => (
               <div
                 key={item.id}
-                className="w-full h-96 bg-transparent ring-4 rounded-lg relative overflow-hidden group cursor-pointer"
+                className="w-full md:h-96 bg-transparent ring-4 rounded-lg relative overflow-hidden group cursor-pointer"
               >
                 <img
                   src={item.url}
                   alt="portfolio image"
                   className="w-full h-full object-cover rounded-lg "
                 />
-                <div className="absolute w-full h-full bg-black/50 -bottom-96 transition-all duration-500 group-hover:-bottom-72 p-5 space-y-3">
-                  <h1>{item.title}</h1>
+                <div className="absolute w-full h-full bg-black/50 -bottom-96 transition-all duration-500 group-hover:-bottom-56 md:group-hover:-bottom-72 p-5 space-y-3">
+                  <h1 className="font-medium">{item.title}</h1>
                   <button
                     className="bg-gradient-to-r from-blue-400 to-purple-500 px-3 py-1 text-sm rounded-2xl "
                     onClick={() => viewHandler(item.id)}
@@ -121,13 +122,23 @@ export default function PortFolio() {
                 <img
                   src={cards[data].url}
                   alt="person image"
-                  className="w-full min-w-36 md:min-w-60 h-full object-contain rounded-lg ring-2"
+                  className="w-full min-w-36 md:w-fit h-full md:h-[30rem] object-contain rounded-lg ring-2"
                 />
               </div>
             </div>
           )}
         </div>
       )}
+      <div className="w-full h-full">
+        <p className="text-sm md:text-base text-gray-500 px-10 md:px-36 text-center">
+          Each entry in our portfolio represents a collaborative journey with
+          our clients, where we&apos;ve leveraged our skills and insights to propel
+          their brands to new heights in the digital world. From captivating
+          social media campaigns to innovative SEO strategies, our portfolio
+          demonstrates the diverse range of services we offer and the tangible
+          impact we&apos;ve made on our clients&apos; businesses.
+        </p>
+      </div>
     </div>
   );
 }
